a8d7d3605e4f477910bc4c5cae09721b29bb4022
Omar Khaled <72626558+omar-vtc@users.noreply.github.com>
2025-06-01 15:40:09 +0300

    advicer bloc and event triggered

--- advicer/lib/app/pages/advice/advice_page.dart
+++ advicer/lib/app/pages/advice/advice_page.dart
@@ -1,9 +1,23 @@
 import 'package:advicer/app/core/services/theme_service.dart';
+import 'package:advicer/app/pages/advice/bloc/advicer_bloc.dart';
 import 'package:advicer/app/pages/advice/widgets/advice_field.dart';
 import 'package:advicer/app/pages/advice/widgets/custom_button.dart';
 import 'package:advicer/app/pages/advice/widgets/error_message.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
+
+class AdvicerPageWrapperProvider extends StatelessWidget {
+  const AdvicerPageWrapperProvider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => AdvicerBloc(),
+      child: const AdvicePage(),
+    );
+  }
+}
 
 class AdvicePage extends StatelessWidget {
   const AdvicePage({super.key});
@@ -31,23 +45,30 @@ class AdvicePage extends StatelessWidget {
           children: [
             Expanded(
               child: Center(
-                child: ErrorMessage(message: "sorry! something went wrong!"),
-
-                /* AdviceField(
-                  advice: "example advice -  your day is good everyday",
-                ),*/
-
-                /*CircularProgressIndicator(
-                  color: themeData.colorScheme.secondary,
-                ),*/
-                /* Text(
-                  "Your Advice is waiting!",
-                  style: themeData.textTheme.headlineLarge,
-                  textAlign: TextAlign.center,
-                ),*/
+                child: BlocBuilder<AdvicerBloc, AdvicerState>(
+                  builder: (context, state) {
+                    if (state is AdvicerInitial) {
+                      return Text(
+                        "Your Advice is waiting!",
+                        style: themeData.textTheme.headlineLarge,
+                        textAlign: TextAlign.center,
+                      );
+                    } else if (state is AdvicerStateLoading) {
+                      return CircularProgressIndicator(
+                        color: themeData.colorScheme.secondary,
+                      );
+                    } else if (state is AdvicerStateLoaded) {
+                      return AdviceField(advice: state.advice);
+                    } else {
+                      return const ErrorMessage(
+                        message: "sorry! something went wrong!",
+                      );
+                    }
+                  },
+                ),
               ),
             ),
-            SizedBox(height: 200, child: Center(child: CustomButton())),
+            const SizedBox(height: 200, child: Center(child: CustomButton())),
           ],
         ),
       ),
--- advicer/lib/app/pages/advice/bloc/advicer_bloc.dart
+++ advicer/lib/app/pages/advice/bloc/advicer_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/widgets.dart';
 import 'package:meta/meta.dart';
 
 part 'advicer_event.dart';
@@ -6,8 +7,19 @@ part 'advicer_state.dart';
 
 class AdvicerBloc extends Bloc<AdvicerEvent, AdvicerState> {
   AdvicerBloc() : super(AdvicerInitial()) {
-    on<AdvicerEvent>((event, emit) {
-      // TODO: implement event handler
+    /*
+      when the button is pressed the first time, the initial state will be replaced
+      with the loading state.
+     */
+    on<AdviceRequestedEvent>((event, emit) async {
+      emit(AdvicerStateLoading());
+      //execute busniss logic... ex: call a function that creates job instance
+
+      // simulating the scenario
+      debugPrint("fake advice triggered!!");
+      await Future.delayed(const Duration(seconds: 2), () {});
+      debugPrint("got advice!!");
+      emit(AdvicerStateLoaded(advice: "fake advide is here!!!!"));
     });
   }
 }
